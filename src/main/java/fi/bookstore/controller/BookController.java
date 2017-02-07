3f4db8c176d62fc53411bb3125b164f2b3db930c
miikkahuuskonen <miikkahuuskonen@Miikka-MacBook-Pro.local>
2017-02-07 12:37:37 +0200

    Added Spring Security

--- src/main/java/fi/bookstore/controller/BookController.java
+++ src/main/java/fi/bookstore/controller/BookController.java
@@ -18,9 +18,14 @@ public class BookController {
 	@Autowired
 	private BookRepository repository;
 	
+	@RequestMapping(value="/login")
+    public String login() {	
+       
+		return "login";
+	}
+	
 	@RequestMapping(value="/booklist", method=RequestMethod.GET) 
-	public String booklist(Model model) {
-		
+	public String booklist(Model model) {	
 		List<Book> books= (List<Book>) repository.findAll();
 		model.addAttribute("books", books);
 	
