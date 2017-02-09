c26ca177a284ce3409a6ebf56b19eaf5b0b7a983
miikkahuuskonen <miikkahuuskonen@Miikka-MacBook-Pro.local>
2017-02-09 17:09:08 +0200

    Added sign-up and users

--- src/main/java/fi/bookstore/BookstoreApplication.java
+++ src/main/java/fi/bookstore/BookstoreApplication.java
@@ -5,18 +5,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import fi.bookstore.model.Book;
 import fi.bookstore.model.BookRepository;
+import fi.bookstore.model.User;
+import fi.bookstore.model.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 	
 	@Bean
-	public CommandLineRunner demo(BookRepository repository) {
+	public CommandLineRunner demo(BookRepository repository, UserRepository urepository) {
 	  
 	 return (args) -> {
 		 Book a = new Book(0, "Jaanin elämä", "Miikka Alatalo", 2016, "isbn1234", 13.37);
@@ -25,6 +31,16 @@ public class BookstoreApplication {
 		 repository.save(a);
 		 repository.save(b);
 		 repository.save(c);
+		 
+		 User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+		 User user2 = new User("admin", "$2a$05$bvIG6Nmid91Mu9RcmmWZfO5HJIMCT8riNW0hEp8f6/FuA2/mHZFpe", "ADMIN");
+		 urepository.save(user1);
+		 urepository.save(user2);
+		 
+		 log.info("fetch all books");
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
+			}
 	   
 	 }; 
 	 
