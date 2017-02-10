e087cb982ba6937912d9dd20a02d46d5e548108f
miikkahuuskonen <miikkahuuskonen@Miikka-MacBook-Pro.local>
2017-02-10 12:07:52 +0200

    Added REST and changed 'addStudent' to 'addBook'

--- src/main/java/fi/bookstore/controller/BookController.java
+++ src/main/java/fi/bookstore/controller/BookController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import fi.bookstore.model.Book;
 import fi.bookstore.model.BookRepository;
@@ -38,16 +40,29 @@ public class BookController {
 		return "redirect:/booklist";
 	}
 	
-	@RequestMapping(value= "addStudent", method = RequestMethod.GET)
+	@RequestMapping(value= "addBook", method = RequestMethod.GET)
 	public String addBook(Model model) {
 		model.addAttribute("book", new Book());
-		return "addStudent";
+		return "addBook";
 	}
 	
 	@RequestMapping(value= "save", method = RequestMethod.POST)
 	public String saveBook(Book book) {
 		repository.save(book);
 		return "redirect:/booklist";
+	}
+	//REST
+	
+	//all books
+	@RequestMapping(value="/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest() {
+		return (List<Book>) repository.findAll();
+	}
+	
+	//book by id
+	@RequestMapping(value="/books/{id}", method = RequestMethod.GET)
+	public @ResponseBody Book findBookRest(@PathVariable("id") Long id) {
+		return repository.findOne(id);
 	}
 	
 	 
--- src/main/java/fi/bookstore/model/Book.java
+++ src/main/java/fi/bookstore/model/Book.java
@@ -4,9 +4,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-public class Book {
+public class Book {	
 	
 	@Id
 	@GeneratedValue (strategy = GenerationType.AUTO)
